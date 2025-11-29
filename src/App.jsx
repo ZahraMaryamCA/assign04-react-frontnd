9f05b44a40eb6fc97944bb3ff3ea47cf2cd170da
Zahra Maryam <zmaryamonca@gmail.com>
2025-11-29 14:43:33 -0500

    Initial React App for Assignment 4

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+function App() {
+  return (
+    <div style={{ padding: "40px", fontFamily: "Arial" }}>
+      <h1>Hello Centennial College!</h1>
+      <p>This is my Assignment 4 React App deployed on Netlify.</p>
+    </div>
+  );
+}
+
+export default App;
